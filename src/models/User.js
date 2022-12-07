b9a9ff4000224b8aef65ffd2230ca53e5c8e2f17
ginaguerrero25 <gina.l.guerrero@gmail.com>
2022-12-07 09:18:56 -0800

    make _id on schema true

--- src/models/User.js
+++ src/models/User.js
@@ -2,24 +2,19 @@ const { Schema, model } = require("mongoose");
 
 const { ObjectId } = Schema.Types;
 
-const schema = new Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-    },
-    password:{
-      type: String,
-      required: True,
-      minLength: 4
-    },
-    decks: [{ ref: "deck", type: ObjectId }]
+const schema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
   },
-  {
-    _id: false
-  }
-);
+  password: {
+    type: String,
+    required: True,
+    minLength: 4,
+  },
+  decks: [{ ref: "deck", type: ObjectId }],
+});
 
 module.exports = model("user", schema);
