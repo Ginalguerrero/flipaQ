21e68f445a0cfc7a01b7bc27124d41afef6d7c8d
ginaguerrero25 <gina.l.guerrero@gmail.com>
2022-12-09 15:29:38 -0800

    change stuff for deploying

--- src/config/conn.js
+++ src/config/conn.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const { connect, connection } = mongoose;
 mongoose.set("strictQuery", true);
 
-const connectionString = process.env.CONN;
+const connectionString = process.env.MONGO_URI;
 
 console.log(connectionString);
 connect(connectionString, {
