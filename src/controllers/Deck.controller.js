8d2e2dc2283415e4ed4bbad8edcd622a86518568
ginaguerrero25 <gina.l.guerrero@gmail.com>
2022-12-09 14:59:47 -0800

    fix implementation bug

--- src/controllers/Deck.controller.js
+++ src/controllers/Deck.controller.js
@@ -12,7 +12,11 @@ const getDecks = async (req, res) => {
       "decks"
     );
     const { decks } = userDecks;
-    res.status(StatusCodes.OK).send({ decks: decks.map(({name}) => {name}) });
+    res.status(StatusCodes.OK).send({
+      decks: decks.map(({ name }) => {
+        name;
+      }),
+    });
   } catch (err) {
     res
       .status(StatusCodes.BAD_REQUEST)
@@ -34,7 +38,11 @@ const getCards = async (req, res) => {
         .send({ message: "This deck is not yours" });
     const deckCards = await Deck.findOne({ _id: deckId }).populate("cards");
     const { cards } = deckCards;
-    res.status(StatusCodes.OK).send({ cards.map(({question, answer}) => {question, answer}) });
+    res.status(StatusCodes.OK).send({
+      cards: cards.map(({ question, answer }) => {
+        question, answer;
+      }),
+    });
   } catch (err) {
     res
       .status(StatusCodes.BAD_REQUEST)
