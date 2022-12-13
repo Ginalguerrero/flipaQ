234ec81fa2f2854f857bf77b6aa12c00eb436a80
ginaguerrero25 <gina.l.guerrero@gmail.com>
2022-12-13 13:08:59 -0800

    routes for react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,35 @@
 import React, { useState } from "react";
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./Login";
 import { Register } from "./Register";
 
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
+  const [currentForm, setCurrentForm] = useState("login");
 
   const toggleForm = (formName) => {
     setCurrentForm(formName);
-  }
+  };
 
   return (
-    <div className="App">
-      {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-      }
-    </div>
+    <BrowserRouter className="App">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            currentForm === "login" ? (
+              <Login onFormSwitch={toggleForm} />
+            ) : (
+              <Register onFormSwitch={toggleForm} />
+            )
+          }
+        />
+        {/* <Route path="/home" element={<HomePage />} /> */}
+
+        {/* <Route path="*" element={<NotFoundPage />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
